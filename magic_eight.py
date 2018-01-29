5a74cd4a75e2fb63ea9a59ca34f0672a420b5dae
Heidi Li <hchiu@umich.edu>
2018-01-29 14:33:33 -0500

    working on while loop

--- magic_eight.py
+++ magic_eight.py
@@ -1,11 +1,15 @@
 import random
 
 def magic_question():
-    question = input("What is your question?")
     list_of_answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+    question = input("What is your question?")
 
-    while question != "quit"
+    while question != "quit":
         if question[-1] == "?":
-            return random.choice(list_of_answers)
+            print (random.choice(list_of_answers))
         else:
-            return "I'm sorry, I can only answer questions."
+            print ("I'm sorry, I can only answer questions.")
+
+
+question = magic_question()
+print(question)
