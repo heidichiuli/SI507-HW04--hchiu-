9ef5c983511ba20677dd9a20688ad21f375ab41f
Amy Austin <austinam@umich.edu>
2018-01-29 18:59:37 -0500

    changes made

--- magic_eight.py
+++ magic_eight.py
@@ -1,15 +1,16 @@
 import random
 
 def magic_question():
-    list_of_answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-    question = input("What is your question?")
-
-    while question != "quit":
-        if question[-1] == "?":
-            print (random.choice(list_of_answers))
+    current_boolean = True
+    while current_boolean == True:
+        question = input("What is your question?")
+        if question == "quit":
+            break
+        elif question[-1] != "?":
+            print("I'm sorry, I can only answer questions.")
         else:
-            print ("I'm sorry, I can only answer questions.")
+            list_of_answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+            print(random.choice(list_of_answers))
+            current_boolean = False
 
-
-question = magic_question()
-print(question)
+magic_question()
